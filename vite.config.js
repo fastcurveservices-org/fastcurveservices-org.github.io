7987815fc000d1958def8b7f4d529d6f92db4a68
ankit.agarwal <ankit.agarwal@fastcurveservices.com>
2025-06-08 12:56:28 +0530

    remove context path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/website/", // 👈 Context path
+  base: "/", // 👈 Context path
   plugins: [react()],
 });
