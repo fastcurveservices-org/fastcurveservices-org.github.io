619a696af5c411ad39b91e10cc912220a74e0360
ankit.agarwal <ankit.agarwal@fastcurveservices.com>
2025-06-08 13:13:06 +0530

    add path prefix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { execSync } from "child_process";
+
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
 // https://vite.dev/config/
 export default defineConfig({
   base: "/", // 👈 Context path
   plugins: [react()],
+  build: {
+    assetsDir: `static/${commitHash}`, // put static assets in versioned folder
+  },
 });
